0058a8788d8755a1272431f543718445127213dc
Nahir Cavallero <nvignolo@uade.edu.ar>
2023-07-08 15:38:13 -0300

    ports

--- Apps_Interactivas_backend/Apps_Interactivas_backend/src/main/java/com/example/Apps_Interactivas_backend/AppsInteractivasBackendApplication.java
+++ Apps_Interactivas_backend/Apps_Interactivas_backend/src/main/java/com/example/Apps_Interactivas_backend/AppsInteractivasBackendApplication.java
@@ -2,6 +2,9 @@ package com.example.Apps_Interactivas_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 public class AppsInteractivasBackendApplication {
@@ -11,3 +14,13 @@ public class AppsInteractivasBackendApplication {
 	}
 
 }
+
+@Component
+public class ServerPortCustomizer implements WebServerFactoryCustomizer<ConfigurableServletWebServerFactory> {
+
+	@Override
+	public void customize(ConfigurableServletWebServerFactory factory) {
+		factory.setPort(8080);
+	}
+}
+
